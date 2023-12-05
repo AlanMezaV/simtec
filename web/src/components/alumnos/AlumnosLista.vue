77ed3fc382db32742e277815b8eb3de782776cb6
Monica <angulosmonica@gmail.com>
2023-12-04 23:48:59 -0700

    vista alumnos

--- web/src/components/alumnos/AlumnosLista.vue
+++ web/src/components/alumnos/AlumnosLista.vue
@@ -1,34 +1,66 @@
 <template>
 	<div class="contenedor">
 		<div class="AlumnosLista">
-			<span>Alumnos</span>
-			<button @click.prevent="nuevoAlumno">Agregar</button>
-			<table>
-				<thead>
-					<tr>
-						<th>Numero de control</th>
-						<th>Nombre</th>
-						<th>Carrera</th>
-						<th>Estatus</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="alumnos in lista_alumnos" :key="alumnos.ncontrol">
-						<td class="espacio">{{ alumnos.ncontrol }}</td>
-						<td class="espacio">{{ alumnos.nombre }}</td>
-						<td class="espacio">{{ alumnos.carrera }}</td>
-						<td class="espacio">{{ alumnos.estatus }}</td>
-						<td class="espacio">
-							<button @click="mostrarOpciones(alumnos)">...</button>
-							<div v-if="alumnos.mostrarOpciones" class="menu-desplegable">
-								<button @click.prevent="editarAlumno(alumnos)">Editar</button>
-								<br />
-								<button @click="eliminarAlumno(alumnos)">Eliminar</button>
-							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+			<div class="contenedor-encabezado">
+				<h2>Alumnos</h2>
+				<button @click.prevent="nuevoAlumno" class="boton-agregar	">
+					<img src="../../../public/images/boton-mas.svg" alt="">
+					Agregar Alumno
+				</button>
+			</div>
+			<div></div>
+			<div class="contenedor-tabla">	
+				<div class="tabla">
+					<div class="tabla-encabezado">
+					<div class="t-header">Numero de control</div>
+					<div class="t-header">Nombre</div>
+					<div class="t-header">Carrera</div>
+					<div class="t-header">Estatus</div>
+					<div class="t-header"></div>
+				</div>
+					
+					<div v-for="alumnos in lista_alumnos" :key="alumnos.ncontrol" class="datos">
+							<span class="espacio">{{ alumnos.ncontrol }}</span>
+							<span class="espacio">{{ alumnos.nombre }}</span>
+							<span class="espacio">{{ alumnos.carrera }}</span>
+							<span class="espacio estatus">{{ alumnos.estatus }}</span>
+							<span class="espacio">
+								<button @click="mostrarOpciones(alumnos)" class="boton-acciones">···</button>
+								<div v-if="alumnos.mostrarOpciones" class="menu-desplegable">
+									<button @click.prevent="editarAlumno(alumnos)">Editar</button>
+									<br />
+									<button @click="eliminarAlumno(alumnos)">Eliminar</button>
+								</div>
+							</span>
+					</div>
+				<!-- <table>
+					<thead>
+						<tr>
+							<th>Numero de control</th>
+							<th>Nombre</th>
+							<th>Carrera</th>
+							<th>Estatus</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="alumnos in lista_alumnos" :key="alumnos.ncontrol">
+							<td class="espacio">{{ alumnos.ncontrol }}</td>
+							<td class="espacio">{{ alumnos.nombre }}</td>
+							<td class="espacio">{{ alumnos.carrera }}</td>
+							<td class="espacio">{{ alumnos.estatus }}</td>
+							<td class="espacio">
+								<button @click="mostrarOpciones(alumnos)">...</button>
+								<div v-if="alumnos.mostrarOpciones" class="menu-desplegable">
+									<button @click.prevent="editarAlumno(alumnos)">Editar</button>
+									<br />
+									<button @click="eliminarAlumno(alumnos)">Eliminar</button>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table> -->
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -78,17 +110,5 @@ export default {
 };
 </script>
 
-<style>
-:root {
-	--white-smoke: #f1f2f4;
-}
-.AlumnosLista {
-	width: 79%;
-	background-color: var(--white-smoke);
-	border-radius: 30px;
-}
-.espacio {
-	padding-right: 25px;
-	padding-left: 20px;
-}
-</style>
+<style global src="../../styles/variables.css"></style>
+<style scoped src="../../styles/alumnos-lista.css"></style>
