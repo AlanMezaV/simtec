78eba86e440ee3febe2785ef6ef298180eb3ea42
AlanMezva <L20170749@culiacan.tecnm.mx>
2023-12-06 23:02:44 -0700

    Validacion en grupos para que maestro no repita hora

--- web/src/components/grupos/FormEditarGrupo.vue
+++ web/src/components/grupos/FormEditarGrupo.vue
@@ -24,9 +24,6 @@
 				<label for="limitealumnos">Limite de alumnos:</label>
 				<input v-model="grupos.limitealumnos" type="text" id="limitealumnos" required />
 				<br />
-				<label for="inscritos">Alumnos inscritos:</label>
-				<input v-model="grupos.inscritos" type="text" id="inscritos" required />
-				<br />
 				<label for="horariolunes">Horario del lunes:</label>
 				<input v-model="grupos.horariolunes" type="text" id="horariolunes" maxlength="11" />
 				<br />
@@ -55,8 +52,7 @@
 import {URL_DATOS} from "@/utils/constants.js";
 import axios from "axios";
 import GruposLista from "./GruposLista.vue";
-import {traeDatos} from "@/utils/peticiones";
-import {obtenerDatos} from "@/utils/peticiones";
+import {traeDatos, obtenerDatos, traeDatosGrupos} from "@/utils/peticiones";
 
 export default {
 	name: "FormEditarGrupo",
@@ -67,12 +63,16 @@ export default {
 		clavegrupo: {
 			type: String,
 		},
+		horaAnterior: {
+			type: String,
+		},
 	},
 	data: function () {
 		return {
 			grupos: [],
 			clavematerias: [],
 			clavemaestros: [],
+			horarioMaestros: [],
 			mostrarError: false,
 			errorMensaje: "",
 		};
@@ -97,8 +97,24 @@ export default {
 				return true;
 			};
 
+			this.horarioMaestros = await traeDatosGrupos("horarioMaestro", this.grupos.clavemaestro);
+			const validaMaestroHorario = () => {
+				let band = true;
+				const hora = this.grupos.horariolunes;
+				this.horarioMaestros.forEach((horario) => {
+					console.log(hora);
+					console.log(horario.horariolunes);
+					if (hora === horario.horariolunes && this.grupos.horariolunes !== this.horaAnterior) {
+						console.log("entro", hora === horario.horariolunes);
+						this.mostrarError = true;
+						this.errorMensaje = "El maestro ya tiene una materia en ese horario.";
+						band = false;
+					}
+				});
+				return band;
+			};
 			try {
-				if (validaDatos()) {
+				if (validaDatos() && validaMaestroHorario()) {
 					const res = await axios.put(URL_DATOS + "/grupos/" + this.clavegrupo, {
 						clavemateria: this.grupos.clavemateria,
 						clavegrupo: this.grupos.clavegrupo,
@@ -112,9 +128,6 @@ export default {
 						horarioviernes: this.grupos.horarioviernes,
 					});
 					this.$router.push("/grupos");
-				} else {
-					this.mostrarError = true;
-					this.errorMensaje = "No debe de haber datos vacios.";
 				}
 			} catch (error) {
 				console.error("Error al verificar la existencia del grupo:", error);
